db4f62fd045488d2a06e848910b7887e28141dc5
Mustafiz898 <mustafiz.ruet.898@gmail.com>
2025-10-27 08:15:50 +0600

    Displaying number with base prefix

---
+++ C_Basics/numberBase02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a = 15;
+
+    // Displaying numbers with Base prefix
+    printf("Decimal: %d\n", a);
+    printf("Octal: %#o\n", a);
+    printf("Hexa decimal: %#x", a);
+
+    return 0;
+}
