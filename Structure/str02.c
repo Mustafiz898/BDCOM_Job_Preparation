9ef94a7d597fd370be119f23c54934cb0fdaedd9
Mustafiz898 <mustafiz.ruet.898@gmail.com>
2025-11-03 09:32:46 +0600

    Array to Pointer modification for author of the book

--- Structure/str02.c
+++ Structure/str02.c
@@ -6,7 +6,7 @@ struct library{
     const char *Book_name;
     int customer_id;
     int rent_no;
-    char author[200];
+    char *author;
 
 };
 
@@ -15,6 +15,11 @@ int main(){
     struct library c2 = {"book2", 102, 2};
     struct library c3 = {"book3", 103, 3};
     struct library c4 ={.Book_name = "book4",.rent_no = 4, .customer_id = 104};
+
+    c1.author = malloc(20);
+    c2.author = malloc(20);
+    c3.author = malloc(20);
+    c4.author = malloc(20);
     
     // author name input 
     strcpy(c1.author, "Author01");
@@ -27,6 +32,12 @@ int main(){
     printf("Book Name: %s Customer Id: %d Rent no.: %d Author: %s\n", c2.Book_name, c2.customer_id, c2.rent_no, c2.author);
     printf("Book Name: %s Customer Id: %d Rent no.: %d Author: %s\n", c3.Book_name, c3.customer_id, c3.rent_no, c3.author);
     printf("Book Name: %s Customer Id: %d Rent no.: %d Author: %s\n", c4.Book_name, c4.customer_id, c4.rent_no, c4.author);
+
+    // Always free allocated memory
+    free(c1.author);
+    free(c2.author);
+    free(c3.author);
+    free(c4.author);
     
     return 0;
 }
