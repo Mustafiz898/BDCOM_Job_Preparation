5827b78e9dcb5987afee975d6815c3d93061c690
Mustafiz898 <mustafiz.ruet.898@gmail.com>
2025-11-05 00:22:53 +0600

    Using DMA instead of static variable to store data from the file

--- File_Handling/reading.c
+++ File_Handling/reading.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     FILE * fptr= NULL;
-    char arr[10];
+    // char arr[10];
+    char * ptr;
+    ptr = (char *)calloc(10, sizeof(char));
 
 
     fptr = fopen("random01", "r");
+    if(fptr == NULL){
+        printf("File not found!");
+        exit(1);
+    }
     
     while(!feof(fptr)){
-        fgets(arr, 10, fptr);
-        printf("%s", arr);
+        fgets(ptr, 10, fptr);
+        printf("%s", ptr);
     }
+
+    free(ptr);
+    fclose(fptr);
     return 0;
 }
