dcb6a1c95c1e6b549bf6afb07f7f75c5cfd4f789
Mustafiz898 <mustafiz.ruet.898@gmail.com>
2025-11-15 23:16:49 +0600

    Binary Search Tree implementation using Linked List and Inorder Traversal

---
+++ DSA/Binary_Tree/basic_Implementation_using_Linked_List.c
@@ -0,0 +1,25 @@
+//_____________TREE______________
+
+/*            10(r1)
+             /  \
+        (r2)5    15(r5)
+           / \   /  
+      (r3)3   7 12(r6)  
+             (r4) 
+
+output using inorder traversal: 3 5 7 10 12 15
+*/               
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node *left, *right;
+}node;
+
+int main(){
+
+
+    return 0;
+}
